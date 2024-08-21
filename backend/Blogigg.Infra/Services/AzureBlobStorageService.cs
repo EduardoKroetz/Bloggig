89b7fd0ad45c31090d35394de60a45f845e3028c
EduardoKroetz <eduardokroetz0@gmail.com>
2024-08-21 17:46:52 -0300

    feat: create azure blob storage service

---
+++ backend/Blogigg.Infra/Services/AzureBlobStorageService.cs
@@ -0,0 +1,62 @@
+﻿
+using Azure.Storage.Blobs;
+using Bloggig.Infra.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace Bloggig.Infra.Services;
+
+public class AzureBlobStorageService : IAzureBlobStorageService
+{
+    private readonly IConfiguration _configuration;
+    private readonly BlobServiceClient _blobServiceClient;
+    private readonly string _blobImageContainerName;
+    private readonly string _blobFileServiceUrl;
+
+    public AzureBlobStorageService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _blobFileServiceUrl = _configuration["Azure:BlobFileServiceUrl"] ?? throw new Exception("Invalid blob file service url");
+        _blobImageContainerName = _configuration["Azure:BlobImagesContainerName"] ?? throw new Exception("Invalid blob images container name");
+        var blobConnectionString = _configuration["Azure:BlobStorageConnectionString"] ?? throw new Exception("Invalid blob storage connection string");
+        _blobServiceClient = new BlobServiceClient(blobConnectionString);
+
+    }
+
+    public async Task<string> UploadProfileImageAsync(string Base64Img, string username)
+    {
+        //Busca o container de imagens
+        var containerClient = _blobServiceClient.GetBlobContainerClient(_blobImageContainerName);
+
+        //Criar o container se ele não existe
+        await containerClient.CreateIfNotExistsAsync();
+
+        //Pegar os bytes da imagem Base64
+        byte[] imageBytes = Convert.FromBase64String(Base64Img);
+
+        //Criar um nome único para o blob
+        var blobStrBuilder = new StringBuilder();
+        blobStrBuilder.Append(username);
+        blobStrBuilder.Append('-');
+        blobStrBuilder.Append(Guid.NewGuid());
+
+        var blobName = blobStrBuilder.ToString();
+        
+        //Carregar os bytes da imagem Base64 para o blob
+        using (var stream = new MemoryStream(imageBytes))
+        {
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            await blobClient.UploadAsync(stream, overwrite: true);
+        }
+
+        //Formatar a url pública da imagem
+        var imgStrBuilder = new StringBuilder()
+            .Append(_blobFileServiceUrl)
+            .Append(_blobImageContainerName)
+            .Append("/")
+            .Append(blobName);
+
+        return imgStrBuilder.ToString();
+    }
+}
---
+++ backend/Blogigg.Infra/Services/Interfaces/IAzureBlobStorageService.cs
@@ -0,0 +1,6 @@
+﻿namespace Bloggig.Infra.Services.Interfaces;
+
+public interface IAzureBlobStorageService
+{
+    Task<string> UploadProfileImageAsync(string Base64Img, string username);
+}
